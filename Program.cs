eb167cdfacbc7b1f31d40ef4f29d5b8413276c4b
Lawliet <rm87818@estudante.fieb.edu.br>
2022-11-06 18:51:37 -0300

    criação do projeto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+    namespace aula02
+    {
+        class program
+        {
+            static void Main(string[] args)
+            {
+                Pessoa obj = new Pessoa();
+                obj.apresentar("Lawliet", 16);
+                
+            }
+        }
+    }
--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 Código Orientação a objetos em c#
+esta é apenas uma área para copiar os códigos mesmo
